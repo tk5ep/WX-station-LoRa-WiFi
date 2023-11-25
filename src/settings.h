2db58566d1b85eb5b61586c8797e69aa5d15f330
Patrick EGLOFF <pegloff@gmail.com>
2023-11-25 06:41:28 +0100

    Update settings.h

--- src/settings.h
+++ src/settings.h
@@ -34,15 +34,15 @@ bool DISPLAY_CARDINAL = false;               // display directions on OLED. true
 /***************************************************
    OLED display
 ***************************************************/
-byte ECOMODE = 1;                           	   // screensaver 0=OFF 1=PERMANENT 2=ONLY BETWEEN TX
+byte ECOMODE = 0;                           	   // screensaver 0=OFF 1=PERMANENT 2=ONLY BETWEEN TX
 
 /***************************************************
    Station coordinates
 ***************************************************/
-#define CALLSIGN "TK5EP-13"                     // callsign with SSID ex TK5EP-10
-#define LATITUDE "4157.27N"                     // latitude in DDMM.MM NORTH ex : 4156.95N
+#define CALLSIGN "YOURCALL-13"                     // callsign with SSID ex TK5EP-10
+#define LATITUDE "4157.27N"                      // latitude in DDMM.MM NORTH ex : 4156.95N
 #define LONGITUDE "00841.95E"                   // longitude in DDDMM.MM EAST ex: 00845.26E
-byte ALTITUDE = 60;                             // home altitude in meters
+byte ALTITUDE = 780;                             // home altitude in meters
 
 /***************************************************
    Sensors
@@ -50,7 +50,6 @@ byte ALTITUDE = 60;                             // home altitude in meters
 uint8_t BME280_I2C = 0x76;                      // BME280 i2c address either 0x76 or 0x77
 const float rainBucketCont = 0.28;              // rain sensor bucket containance in mm of water, depends on model
 const byte RAINDEBOUNCE = 250;                   // in ms debounce time for the rain sensor
-//const byte WINDDEBOUNCE = 200;                 // in ms debounce time for the anemometer
 
 // ModBus wind sensors
 uint8_t NewSensorAddress = 0x02;                // put here the new sensor address in case of needed change. Certainly one of two addresses below. Be sure to have only one sensor on ModBus. 
@@ -68,28 +67,28 @@ int TXPOWER = 20;                               // power in dBm
 /***************************************************
    WIFI
 ***************************************************/
-const char* ssid =              "TKNET";                 // Change this to your WiFi SSID
-const char* password =          "RADIOAMATEUR$CORSICA";  // Change this to your WiFi password
+const char* ssid =              "YOURSSID";                 // Change this to your WiFi SSID
+const char* password =          "YOURPASSWORD";  // Change this to your WiFi password
 bool WITH_STATIC_IP = false;			  			            // true=static address or false=DHCP
-IPAddress local_IP(44, 168, 80, 141);		  	            // Set your Static IP address like xxx,xxx,xxx,xxx
-IPAddress gateway(44, 168, 80, 129);		  	            // Set your Gateway IP address
-IPAddress subnet(255, 255, 255, 240);			            // Set your subnet mask
-IPAddress primaryDNS(44, 168, 80, 129);		  	         // optional, put 8.8.8.8 if you don't know
-//IPAddress secondaryDNS(8, 8, 4, 4); 		  	              // optional
+IPAddress local_IP(00, 000, 00, 000);		  	            // Set your Static IP address like xxx,xxx,xxx,xxx
+IPAddress gateway(00, 000, 00, 254);		  	            // Set your Gateway IP address
+IPAddress subnet(255, 255, 255, 0);			            // Set your subnet mask
+IPAddress primaryDNS(00, 000, 00, 254);		  	         // optional, put 8.8.8.8 if you don't know
+//IPAddress secondaryDNS(8, 8, 8, 8); 		  	              // optional
 
 /***************************************************
    WUNDERGROUND
 ***************************************************/
 const char* WG_server =         "weatherstation.wunderground.com";   // Wunderground server address
-const char* WG_ID =             "ICORSEAJ5";                         // Your WunderGround ID. You need to register to get this ID and Key
-const char* WG_PWD =            "cdab4536";                          // Your WunderGround key
+const char* WG_ID =             "YOURID";                         // Your WunderGround ID. You need to register to get this ID and Key
+const char* WG_PWD =            "YOURPWD";                          // Your WunderGround key
 
 /***************************************************
    MQTT Broker
 ***************************************************/
 //const char *mqtt_broker = "test.mosquitto.org";
 //const char *mqtt_broker = "89.234.155.3";
-const char* mqtt_broker =       "iot.radioamateur.corsica";		// the MQTT server
+const char* mqtt_broker =       "yourbrokeraddress";		// the MQTT server
 const int mqtt_port = 1883;									         // MQTT server port 1883 by default
 const char* mqtt_username =     "";								      // MQTT server username if needed, leave blank if not
 const char* mqtt_password =     "";								      // MQTT server password if needed, leave blank if not
@@ -106,14 +105,14 @@ const char* TOPIC_RAIN24H =     "tk5ep/qra/rain24h";			   // topic for the 24 h 
 /***************************************************
    APRS-IS
 ***************************************************/
-const char *APRS_IS_CALLSIGN =  "TK5EP-13";					      // APRS IS callsign with SSID, normally -13
-const char *APRS_IS_PASSWD =    "17132";						      // APRS IS password, get it at https://apps.magicbug.co.uk/passcode/
+const char *APRS_IS_CALLSIGN =  "YOURCALL-13";					      // APRS IS callsign with SSID, normally -13
+const char *APRS_IS_PASSWD =    "11111";						      // APRS IS password, get it at https://apps.magicbug.co.uk/passcode/
 //const char *APRS_IS_SERVER = "rotate.aprs.net";				   // APRS server or server pool
-const char *APRS_IS_SERVER =    "aprs-public.radioamateur.corsica"; // APRS server address
+const char *APRS_IS_SERVER =    "youraprsserver"; // APRS server address
 const int APRS_IS_SERVER_PORT = 14580;						         // APRS server port, default 14580
 
 /***************************************************
    OTA
 ***************************************************/
 const char* OTA_username =          "root";                 // OTA access login
-const char* OTA_password =          "tk5kp";                // OTA access password
+const char* OTA_password =          "password";                // OTA access password
